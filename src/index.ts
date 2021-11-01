199d482d5571276db6f502b611629758c981c749
matt <mattsnaccc@gmail.com>
2021-11-01 00:40:20 -0700

    fix(index): export all the components

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,9 @@ export type {MessageProps, MessageProviderProps} from './message';
 
 export {default as Link} from './link';
 export type {LinkProps} from './link';
+
+export {default as Notification} from './notification';
+export type {NotificationProps} from './notification';
+
+export {default as Image} from './image';
+export type {ImageProps} from './image';
